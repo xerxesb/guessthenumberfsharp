c0656d1afa7fc67bc09f222384529d9dec0f8c4f
Xerxes Battiwalla <xbattiwalla@cochlear.com>
2013-03-16 23:03:02 +1100

    Updated to use parameterized active patterns

--- Program.fs
+++ Program.fs
@@ -9,24 +9,49 @@ type Game = {
     NumberOfGuesses : int
 }
 
+type Guess =
+    | Low
+    | Equal
+    | High
+
 
 let getGuess () =
     stdout.Write "Guess the number: "
     stdin.ReadLine() |> int
 
 
-let getDesc guess actual =
-    match guess with
-    | x when x > actual -> printfn "Too high!" |> ignore 
-    | x when x < actual -> printfn "Too low!" |> ignore 
-    | _ -> printfn "Correct" |> ignore
+let (|GuessType|_|) (guess :int) (actual : int) =
+    if (guess < actual) then Some Low
+    elif (guess > actual) then Some High
+    else Some Equal
+ 
 
+let getDesc guess actual =
+    match actual with
+    | GuessType guess Low -> printfn "Too low!" |> ignore
+    | GuessType guess High -> printfn "Too high!" |> ignore
+    | GuessType guess Equal -> printfn "Correct!" |> ignore
+    | _ -> ()
+
+
+(*
+//example of parameterized active pattern
+let (|MultipleOf|_|) (multiplicand : int) (n : int) =
+    Some (n / multiplicand, n % multiplicand)
+
+
+let mult (n : int) (x :int) =
+    match n with
+    | MultipleOf x (_,0) -> printfn "%A is multiple of %A" n x
+    | MultipleOf x (m,_) -> printfn "%A is not multiple of %A" n x
+    | _ -> ()
+*)
 
 let rec runGame game =
     if (game.GuessedNumber = game.ActualNumber) then game
     else
-        getDesc(game.GuessedNumber, game.ActualNumber) |> ignore
         let nextIter = { game with GuessedNumber = getGuess (); NumberOfGuesses = game.NumberOfGuesses + 1 }
+        getDesc nextIter.GuessedNumber nextIter.ActualNumber
         runGame nextIter 
 
 
